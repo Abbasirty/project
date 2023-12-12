c80f270e198559a257294ace8e63ce2ac5a7867f
Abbasirty <153621095+Abbasirty@users.noreply.github.com>
2023-12-12 16:47:13 +0330

    project

---
+++ project.py
@@ -0,0 +1,186 @@
+a=input("kala-name: (noodle soup   potatoes(chips)   macaroni   lasagna   fish  tuna   salad  pizza  conserves   youghurt)")
+a=input("production-date: (1401/1/1)")
+a=input("expiration-period date: (1402)")
+a=input("price:  38'000'000 (for all kala)")
+class mavadghazaei:
+    def __init__(self):
+      self.code_kala =("156789")
+      self.kala_name= ("noodle soup  potatoes  macaroni lasagna  fish  tuna   salad  pizza  conserve  youghurt")
+      self.company_producer = ("Markazi  (companey")
+      self.production_date = ("1401/1/1")
+      self.expiration_period_date = ("3 to 4 mounth or up to 1 year(1402)")
+      self.number_items_purchased = ("990 digit")
+      self. number_to_stock = ("890 digit")
+      self.price =  ("38'000'000 (for all kala)")
+      
+a=input("takhfif  snap pay: 50%")
+class hessab__karbari_moshtarian:
+     def __init__(self):
+         self.karbari_name = ("Admin")
+         self.firstname_lastname = ("Amir hossein yazdanian")
+         self.National_ID_number_code = ("1245678911")
+         self.phone_numbers = ("9124567891")
+         self.Total_purchase_amount =  ("76'000'000") 
+         self.Discount_amount = ("50 % takhfif all (amount saved and recovered)") 
+         self.mablaghe_bedehi = ("nadarad")
+         
+a=input("companey  producer  code:  Admin License 12457890")
+a=input("code kala:  (156789)")
+class sefareshat__Markazi__companey__sefareshat__moshtarian:
+         def __init__(self):
+             self.code_sefaresh = ("141356")
+             self.karbari_name_karbar = ("Admin134567")
+             self.companey_producer_code = ("Admin Licence 12457890")
+             self.code_kala =("156789")
+             self.tedad_kala_order = ("1000 digit")
+             self.date_order = ("1403/1/1")
+             self.date_order_register = ("1402/1/1")
+             self.date_order_delivery = ("1403/2/1")
+             
+a=input("companey-code:  (23478901234)")
+a=input("companey-name:  (shahrvand market)")
+a=input("the  number  of  user  orders  not  delivered:  234 digit")
+a=input("mablaghe-foroosh:  3'000'000'000 (for all kala)")
+class all__companey__under__the__collection:
+       def __init__(self):
+        self.companey_code = ("23478901234")
+        self.companey_name = (" shahrvand market")
+        self.the_number_of_user_orders_not_delivered = ("234 digit")
+        self.mablaghe_foroosh = ("3'000'000'000 (for all kala)")
+
+input("sales-analysis : ")
+input("customer-club :")
+class activities__for__this__system :
+     def __init__(self):
+          self.new_karbar_registration =  new_karbar_registration 
+          self.new_companey_registration = new_companey_registration 
+          self.change_profile = change_profile 
+          self.change_companey_details = companey_details 
+          self.new_mavadghazaei_for_supply = new_mavadghazaei_for_supply 
+          self.change_Specifications_mavadghazaei = change_Specifications_mavadghazaei
+          self.Registration_of_order_delivery = Registration_of_order_delivery
+          self.sales_analysis_customer_club = analysis_customer_club
+
+
+
+
+input("ends  of  mounths:   calculate  speciall + products")
+input("ends  of  mounths:   calculate  tedad  foroosh-1speciallist + product")
+input("tedad  not  enough:  do not supply")
+input("saved  and  recovered  all  of  these  calculates  and  amounts in this project")                                       
+class tahlile__foroosh:
+      def __init__(self):
+          self.ends_of_mounths = ("calculate  speciall + products")
+          self.ends_of_mounths =  ("calculate  tedad  foroosh_1speciallist + product")
+          self. tedad_not_enough = ("do not supply")
+          self.saved_and_recovered_all_of_these_calculates_and_amounts_in_this_project = ("saved and  recovered  all of  these calculates"
+                                                                                                                                           "and amounts in this project")
+
+
+
+
+
+input("for-takhfif:  for 1'000'000 pay 70% takhfif")
+input("for-another takhfif:  for 5'000'000 pay 50% takhfif")
+input("takhfif for-another pay:  for another pay 20% takhfif")
+input("takhfif  for-use snap pay:   for  use snap pay 60% takhfif")
+input("takhfif for-use etebari payment:  for use etebari payment 10% takhfif")
+input("etebari-payment:   20'000'000 pay's  (saved)")
+input("amount-pay:  12'000'000 for each product you want to buy it")
+input("takhfif  for  amount:  17'000'000-3000'000'000 for each product you want to buy it")
+input("takhfif  for  comments-product:  20'000'000 for 1 each speciall product you want to buy such as sauce or lasagna "
+                                                                      " saved and recovered--hesab karbari  ")
+input("comments for-future to buy products: when you buy the good or best products  you should use the best companey") 
+                                                                                  
+input("saved and recovered all of this amounts in project: saved and recovered  all  of this  amounts  in project")  
+                                                                                                                            
+class customer_club :
+     def __init__(self):
+          self.for_takhfif = ("for 1'000'000 pay 70% takhfif")
+          self.for_another_takhfif = ("for 5'000'000 pay 50% takhfif")
+          self.takhfif_for_another_pay = ("for another pay 20% takhfif")
+          self.takhfif_for_use_snap_pay = (" for  use snap pay 60% takhfif")
+          self.takhfif_for_use_etebari_payment = (" for use etebari payment 10% takhfif")
+          self.etebari_payment = (" 20'000'000 pay's  (saved) ")
+          self.amount_pay = (" 12'000'000 for each product you want to buy it")
+          self.takhfif_for_amount = (" 17'000'000-3'000'000'000 for each product you want to buy it")
+          self.takhfif_for_comments_product =  ( "20'000'000 for 1 each speciall product you want to buy such as sauce or lasagna "
+                                                                      " saved and recovered-hesab karbari")
+          self.comments_for_future_to_buy_products = ( "when you buy the good or best products  you should use the best companey " )
+                                                                               
+          self.saved_and_recovered_all_of_this_amounts_and_all_of_this_in_project =("saved and recovered  all of  this amounts in project" ) 
+                                                                                                                            
+
+
+
+input("add-managements to these participation companey: add 1 management  to working  properly in these companey")
+input("add-recepetion managements to  these companey: add 3 or 4 managements to working  properly in these companey")
+input("add-1 better reception management to this companey: add 1 reception management and try to find better receptionist")
+                                                                                                           
+
+          
+class for__managements__of_these_companey:
+      def __init__(self):
+        self.add_managements_to_these_participation_companey = ("add 1 management  to working  properly in these companey")
+        self.add_recepetion_managements_to_these_companey = (" add 3 or 4 managements to working  properly in these companey") 
+        self.add_1_better_reception_management_to_this_companey = (" add 1 reception management and try to find better receptionist")
+                                                                                                             
+             
+
+             
+             
+             
+
+
+
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+
